71d35935b8e82bb32567580e0e0d5f279cad20c2
Btenks <btenks95@gmail.com>
2024-08-25 10:26:05 +0200

    Number 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 class Bank {
     private String accountType;
     private double accountBalance;
+
+// Number 2
+    public double deposit(double amount) {
+        accountBalance += amount;
+        return accountBalance;
+    }
 }
 public class Main {
 
